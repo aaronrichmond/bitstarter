9669663288fe7068a8bd08a9a34f88fb0457b6d5
Ubuntu <ubuntu@ip-172-31-6-234.us-west-2.compute.internal>
2013-07-12 05:55:53 +0000

    adding read file

--- web.js
+++ web.js
@@ -2,8 +2,21 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
+fs = require('fs')
+
+var content;
+
+fs.readFile('./index.html', 'utf8', function (err,data) {
+  if (err) {
+    return console.log(err);
+  }
+ 
+  content = data;
+  console.log(content);
+});
+
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  response.send('Hello World 2!' + content);
 });
 
 var port = process.env.PORT || 5000;
